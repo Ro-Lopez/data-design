5514163d9b6e386405cf8f78ad8027acdfaab794
Robbie Lopez <rolopez.email@gmail.com>
2017-04-13 08:26:40 -0600

    basic layout - with copied code from first draft

---
+++ epic/conceptual-model.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+
+		<title>
+			Etsy Site
+		</title>
+
+	</head>
+	<body>
+
+		<header>
+			<h1>A Very Bad Etsy Site</h1>
+		</header>
+
+		<main>
+			<h2>User Story:</h2> As a user, I want to favorite a product.
+
+			<h2>Conceptual Model</h2>
+			<ul>
+				<li>one consumer can favorite many products</li>
+				<li>many consumers can favorite many products</li>
+				<li>consumer can unfavorite a product</li>
+				<li>consumers can see how many consumers have favorited a product</li>
+			</ul>
+
+			<h2>Persona</h2>
+			<ul>
+				<li>college student looking for a product they are saving up for</li>
+				<li>consumer comparing product and prices</li>
+			</ul>
+
+			<h2>Use-Case</h2>
+			<ol>
+				<li>Consumer creates account</li>
+				<li>Consumer searches for products they are interested in</li>
+				<li>Consumer clicks favorite to holds a record of the product</li>
+				<li>Product gets stored to bring back in the future</li>
+			</ol>
+
+		</main>
+	</body>
+</html>
