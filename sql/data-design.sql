dc6cb714697a7be921d39f9ed067e8bda175977d
Robbie Lopez <rolopez.email@gmail.com>
2017-04-15 14:27:08 -0600

    first attempt of with all 3 tables

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -1,34 +1,85 @@
+
+
+
 DROP TABLE IF EXISTS favorite;
 DROP TABLE IF EXISTS product;
 DROP TABLE IF EXISTS profile;
-
 
 CREATE TABLE profile (
 
 profileID INT UNSIGNED AUTO_INCREMENT NOT NULL,
 
---NOT SURE IF WE NEED THIS
---profileActivationToken char(32)
+-- NOT SURE IF WE NEED THIS
+-- profileActivationToken char(32)
 
---what does this have to do with favoriting a product?
-profileHandle VARCHAR (32)
+-- what does this have to do with favoriting a product?
+-- profileHandle VARCHAR(32)
 
-profileEmail VARCHAR (128) UNIQUE NOT NULL,
+profileEmail VARCHAR(128) UNIQUE NOT NULL,
 
 profileHash CHAR(128) NOT NULL,
 
---I do not think we need this?
-profilePhone VARCHAR (32),
+-- I do not think we need this?
+profilePhone VARCHAR(32),
 
-profileSalt CHAR (64 NOT NULL,
+profileSalt CHAR(64) NOT NULL,
 
-UNIQUE(profileEmail),
+UNIQUE (profileEmail),
 
---again what does this have to do with favoriting a product?
-UNIQUE (profileHandle),
+-- again what does this have to do with favoriting a product?
+-- UNIQUE (profileHandle),
 
-PRIMARY KEY (profileId),
+PRIMARY KEY (profileId)
 
 );
+
+
+CREATE TABLE product (
+
+	productId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+
+	productProfileId INT UNSIGNED NOT NULL,
+
+	productContent VARCHAR(140),
+
+	productDate DATETIME NOT NULL,
+
+	INDEX (productProfileId),
+
+	FOREIGN KEY(productProfileId) REFERENCES profile(profileId),
+
+	PRIMARY KEY(productId)
+
+);
+
+CREATE TABLE favorite (
+
+	favoriteProfileId INT UNSIGNED NOT NULL,
+
+	favoriteProductId INT UNSIGNED NOT NULL,
+
+	favoriteDate DATETIME NOT NULL,
+
+	INDEX (favoriteProfileId),
+
+	INDEX (favoriteProductId),
+
+	FOREIGN KEY (favoriteProfileId) REFERENCES profile(profileId),
+
+	FOREIGN KEY (favoriteProductId) REFERENCES product(productId),
+
+	PRIMARY KEY(favoriteProfileId, favoriteProductId)
+
+
+);
+
+
+
+
+
+
+
+
+
 
 
