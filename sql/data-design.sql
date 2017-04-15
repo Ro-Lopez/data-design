11c973a34bd3f33134ae56efa4839a99a327a1f0
Robbie Lopez <rolopez.email@gmail.com>
2017-04-15 16:26:12 -0600

    works after I successfully connected the mysql ssh

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -1,39 +1,72 @@
+
+
 DROP TABLE IF EXISTS favorite;
 DROP TABLE IF EXISTS product;
 DROP TABLE IF EXISTS profile;
 
+
 CREATE TABLE profile (
+
 	profileId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+
 	profileActivationToken char(32),
+
 	profileAtHandle VARCHAR(32)NOT NULL,
+
 	profileEmail VARCHAR(128) UNIQUE NOT NULL,
+
 	profileHash CHAR(128) NOT NULL,
+
 	profilePhone VARCHAR(32),
+
 	profileSalt CHAR(64) NOT NULL,
+
 	UNIQUE(profileEmail),
+
 	UNIQUE(profileAtHandle),
+
 	PRIMARY KEY(profileId)
+
 );
+
 
 CREATE TABLE product (
+
 	productId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+
 	productProfileId INT UNSIGNED NOT NULL,
+
 	productContent VARCHAR(140) NOT NULL,
+
 	productDate DATETIME NOT NULL,
+
 	INDEX(productProfileId),
+
 	FOREIGN KEY(productProfileId) REFERENCES profile(profileId),
+
 	PRIMARY KEY(productId)
+
 );
 
+
 CREATE TABLE favorite (
+
 	favoriteProfileId INT UNSIGNED NOT NULL,
+
 	favoriteProductId INT UNSIGNED NOT NULL,
+
 	favoriteDate DATETIME NOT NULL,
+
 	INDEX(favoriteProfileId),
+
 	INDEX(favoriteProductId),
+
 	FOREIGN KEY(favoriteProfileId) REFERENCES profile(profileId),
+
 	FOREIGN KEY(favoriteProductId) REFERENCES product(productId),
+
 	PRIMARY KEY(favoriteProfileId, favoriteProductId)
+
 );
 
 
