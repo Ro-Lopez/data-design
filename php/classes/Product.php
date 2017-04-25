64873cbbeb097995a1aa743d0bdc0fba05b476eb
Robbie Lopez <rolopez.email@gmail.com>
2017-04-25 11:32:03 -0600

    completed profile, product, and favorite, i hope - almost lunch

--- php/classes/Product.php
+++ php/classes/Product.php
@@ -39,7 +39,17 @@ class Product implements \JsonSerializable {
 	 **/
 	private $productDate;
 
-	//constructor of the product
+	/**
+	 * constructor for this Product
+	 * @param int|null $newProductId id of this Product or null if a new Product
+	 * @param int $newProductProfileId id of the Profile that sent this Product
+	 * @param string $newProductContent string containing actual product data
+	 * @param \DateTime|string|null $newProductDate date and time Product was sent or null if set to current date and time
+	 * @throws \InvalidArgumentException if data types are not valid
+	 * @throws \RangeException if data values are out of bounds (e.g., strings too long, negative integers)
+	 * @throws \TypeError if data types violate type hints
+	 * @throws \Exception if some other exception occurs
+	 **/
 	public function _construct(?int $newProductId, int $newProductProfileId, string $newProductContent, $newProductDate = null) {
 		try {
 			$this->setProductId($newProductId);
@@ -55,13 +65,20 @@ class Product implements \JsonSerializable {
 		}
 	}
 
-
-	//accessor method for product id
+	/**
+	*accessor method for product id
+	*@int|null value of product id
+	**/
 	public function getProductId(): ?int {
 		return ($this->productId);
 	}
 
-	//mutator method for product id
+	/**
+	 * mutator method for product id
+	 * @param int|null $newProductId new value of product id
+	 * @throws \RangeException if $newProductId is not positive
+	 * @throws \TypeError if $newProductId is not an integer
+	 **/
 	public function setProductId(?int $newProductId): void {
 		if($newProductId === null) {
 			$this->productId = null;
@@ -77,12 +94,20 @@ class Product implements \JsonSerializable {
 	}
 
 
-	//accessor method for product profile id
+	/**
+	*@return int value of the product profile
+	*accessor method for product profile id
+	 **/
 	public function getProductProfileId(): int {
 		return ($this->productProfileId);
 	}
 
-	//mutator method for product profile id
+	/**
+	 * mutator method for product profile id
+	 * @param int $newProductProfileId new value of product profile id
+	 * @throws \RangeException if $newProfileId is not positive
+	 * @throws \TypeError if $newProfileId is not an integer
+	 **/
 	public function setProductProfileId(int $newProductProfileId): void {
 		//verify profile id is positive
 		if($newProductProfileId <= 0) {
@@ -93,12 +118,21 @@ class Product implements \JsonSerializable {
 	}
 
 
-	//accessor method for product content
+	/**
+	*accessor method for product content
+	*@return string value of product content
+	**/
 	public function getProductContent(): string {
 		return ($this->productContent);
 	}
 
-	//mutator method for product content
+	/**
+	 * mutator method for product content
+	 * @param string $newProductContent new value of product content
+	 * @throws \InvalidArgumentException if $newProductContent is not a string or insecure
+	 * @throws \RangeException if $newProductContent is > 140 characters
+	 * @throws \TypeError if $newTProductContent is not a string
+	 **/
 	public function setProductContent(string $newProductContent): void {
 		//verify product content is secure
 		$newProductContent = trim($newProductContent);
@@ -115,13 +149,20 @@ class Product implements \JsonSerializable {
 		$this->productContent = $newProductContent;
 	}
 
-
-	//accessor method for product date
+	/**
+	* accessor method for product date
+	* @return \Datetime val of product date
+	 **/
 	public function getProductDate(): \DateTime {
 		return ($this->productDate);
 	}
 
-	//mutator for product date
+	/**
+	 * mutator method for product date
+	 * @param \DateTime|string|null $newProductDate product date as a DateTime object or string (or null to load the current time)
+	 * @throws \InvalidArgumentException if $newProductDate is not a valid object or string
+	 * @throws \RangeException if $newProductDate is a date that does not exist
+	 **/
 	public function setProductDate($newProductDate = null): void {
 		//if date is null, use current date and time
 		if($newProductDate === null) {
@@ -139,7 +180,12 @@ class Product implements \JsonSerializable {
 	}
 
 
-	//insert this Product into mySQL
+	/**
+	 * inserts this Product into mySQL
+	 * @param \PDO $pdo PDO connection object
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
 	public function insert(\PDO $pdo): void {
 		//enforce the productId is null, do not insert a tweet that already exists
 		if($this->productId !== null) {
@@ -159,7 +205,12 @@ class Product implements \JsonSerializable {
 	}
 
 
-	//deletes this Product from mySQL
+	/**
+	 * deletes this Product from mySQL
+	 * @param \PDO $pdo PDO connection object
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
 	public function delete(\PDO $pdo): void {
 
 		//enforce the productId is not null, do not delete product that has not been inserted
@@ -177,7 +228,12 @@ class Product implements \JsonSerializable {
 	}
 
 
-	//update this Product in mySQL
+	/**
+	 * updates this Product in mySQL
+	 * @param \PDO $pdo PDO connection object
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
 	public function update(\PDO $pdo): void {
 		//enforce productId is not null, do not update a tweet that has not been inserted
 		if($this->productId === null) {
@@ -194,7 +250,14 @@ class Product implements \JsonSerializable {
 		$statement->execute($parameters);
 	}
 
-		//gets a product by productId
+	/**
+	 * gets the Product by productId
+	 * @param \PDO $pdo PDO connection object
+	 * @param int $productId product id to search for
+	 * @return Product|null Product found or null if not found
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError when variables are not the correct data type
+	 **/
 		public static function getProductByProductId(\PDO $pdo, int $productId) : ?Product {
 			//sanitize tht productId before searching
 			if($productId <= 0) {
@@ -226,6 +289,15 @@ class Product implements \JsonSerializable {
 		}
 
 
+
+	/**
+	 * gets the Product by profile id
+	 * @param \PDO $pdo PDO connection object
+	 * @param int $productProfileId profile id to search by
+	 * @return \SplFixedArray SplFixedArray of Products found
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError when variables are not the correct data type
+	 **/
 		public static function getProductByProductProfileId(\PDO $pdo, int $productProfileId) : \SplFixedArray {
 			//sanitize the profile id before searching
 			if($productProfileId <= 0) {
@@ -257,7 +329,15 @@ class Product implements \JsonSerializable {
 		}
 
 
-		//gets Products by content
+
+	/**
+	 * gets the Product by content
+	 * @param \PDO $pdo PDO connection object
+	 * @param string $productContent product content to search for
+	 * @return \SplFixedArray SplFixedArray of Products found
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError when variables are not the correct data type
+	 **/
 		public static function getProductsByProductContent(\PDO $pdo, string $productContent) : \SplFixedArray {
 			//sanitize the description before searching
 			$productContent = trim($productContent);
@@ -292,7 +372,68 @@ class Product implements \JsonSerializable {
 		}
 
 
-		//get all the Products
+	/**
+	 * gets an array of products based on its site
+	 * optional get by method and only added on certain cases
+	 * @param \PDO $pdo connection object
+	 * @param \DateTime $sunriseProductDate beginning date to search for
+	 * @param \Datetime $sunsetProductDate ending date to search for
+	 * @return \SplFixedArray of products found
+	 * @throws \PDOException wheh mySQL related errors occur
+	 * @throws \TypeError when variables are not the correct data type
+	 * @throws \InvalidArgumentException if either sun dates are in the wrong format
+	 */
+	public static function getProductByProductDate (\PDO $pdo, \DateTime $sunriseProductDate, \DateTime $sunsetProductDate ) : \SplFixedArray {
+		//enforce both date are present
+		if((empty ($sunriseProductDate) === true) || (empty($sunsetProductDate) === true)) {
+			throw (new \InvalidArgumentException("dates are empty of insecure"));
+		}
+
+		//ensure both dates are in the correct format and are secure
+		try {
+				$sunriseProductDate = self::validateDateTime($sunriseProductDate);
+				$sunsetProductDate = self::validateDateTime($sunsetProductDate);
+		} catch(\InvalidArgumentException | \RangeException $exception) {
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
+
+		//create query template
+		$query = "SELECT productId, productProfileId, productContent, productDate from product WHERE productDate >= :sunriseProductDate AND productDate <= :sunsetProductDate";
+		$statement = $pdo->prepare($query);
+
+		//format the dates so that ,ySQL can use them
+		$formattedSunriseDate = $sunriseProductDate->format("Y-m-d H:i:s.u");
+		$formattedSunsetDate = $sunsetProductDate->format("Y-m-d H:i:s.u");
+
+		$parameters = ["$sunriseProductDate" => $formattedSunriseDate, "sunsetProductDate" => $formattedSunsetDate];
+		$statement->execute($parameters);
+
+		//build an array of products
+		$products = new \SplFixedArray($statement->rowCount());
+		$statement->setFetchMode(\PDO::FETCH_ASSOC);
+		while(($row = $statement->fetch()) !== false) {
+			try {
+					$product = new Product($row["productId"], $row["productProfileId"], $row["productContent"], $row["productDate"]);
+					$products[$products->key()] = $product;
+					$products->next();
+			} catch(\Exception $exception) {
+					throw (new \PDOException($exception->getCode(), 0, $exception));
+			}
+		}
+		return($products);
+
+	}
+
+
+
+	/**
+	 * gets all Products
+	 * @param \PDO $pdo PDO connection object
+	 * @return \SplFixedArray SplFixedArray of Products found or null if not found
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError when variables are not the correct data type
+	 **/
 		public static function getAllProducts(\PDO $pdo) : \SplFixedArray {
 			//create query template
 			$query = "SELECT productId, productProfileId, productContent, productDate FROM product";
@@ -316,8 +457,10 @@ class Product implements \JsonSerializable {
 		}
 
 
+		/**
 		//formats the state variables for JSON serialization
 		//@return array resulting state variables to serialize
+		**/
 		public function jsonSerialize() {
 			$fields = get_object_vars($this);
 			//format the date so that the front end can consume it
